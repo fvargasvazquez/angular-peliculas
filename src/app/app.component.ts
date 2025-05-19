2967c5ca9139459a3f9057efcfbf7b6f2a4ecb6f
Francisco Vargas <francisco.vargas@elektra.com.mx>
2025-05-19 17:07:40 -0600

    Iniciando clase 32

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,6 +53,10 @@ export class AppComponent implements OnInit {
 
   peliculasEnCine!: any[];
   peliculasProximosEstrenos!: any[];
+
+  clickBoton() {
+    alert('Me has clickeado');
+  }
 }
 
 
--- src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -7,47 +7,22 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css'
 })
-export class ListadoPeliculasComponent implements OnInit {
-ngOnInit(): void {
-    setTimeout(() => {
-      this.peliculas = [{
-        titulo: 'Inside Out 2',
-        fechaLanzamiento: new Date(),
-        precio: 1400.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
-      },
-      {
-        titulo: 'Moana 2',
-        fechaLanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
-      },
-      {
-        titulo: 'Bad Boys: Ride or Die',
-        fechaLanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
-      },
-      {
-        titulo: 'Deadpool & Wolverine',
-        fechaLanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
-      },
-      {
-        titulo: 'Oppenheimer',
-        fechaLanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
-      },
-      {
-        titulo: 'The Flash',
-        fechaLanzamiento: new Date('2016-05-03'),
-        precio: 300.99,
-        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
-      }];
-    }, 2000);
-  }
-@Input({required: true})
+export class ListadoPeliculasComponent {
+
+  @Input({ required: true })
   peliculas!: any[];
+
+  agregarPelicula() {
+    this.peliculas.push({
+      titulo: 'Inception',
+      fechaLanzamiento: new Date('2012-07-03'),
+      precio: 500
+    });
+  }
+
+  remover(pelicula: any) {
+    const indice = this.peliculas.findIndex((peliculaActual: any) => peliculaActual.titulo === pelicula.titulo);
+    console.log("Indice" + indice);
+    this.peliculas.splice(indice, 1);
+  }
 }
